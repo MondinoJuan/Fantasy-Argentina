3747b9cc3ec715530ad33cc0ea5a5d15b1822389
Juan Cruz Mondino <juancm.2000@hotmail.com>
2026-02-13 14:47:15 -0300

    Creo los componentes de login, signup y landing page.

--- Frontend/Fantasy-Argentina/src/app/app.routes.ts
+++ Frontend/Fantasy-Argentina/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './pages/log-in/log-in.component.js';
+import { SignUpComponent } from './pages/sign-up/sign-up.component.js';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'logIn', pathMatch: 'full'},
+    {path: 'logIn', component: LogInComponent},
+    {path: 'signUp', component: SignUpComponent},
+    {path: 'landingPage', component: LandingPageComponent},
+];
+
+export const routingComponents = [
+    LogInComponent,
+    SignUpComponent,
+    LandingPageComponent,
+];
