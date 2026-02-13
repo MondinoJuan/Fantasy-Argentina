b341995dccde7c40352328b4a2317c42c287b662
Juan Cruz Mondino <58336253+MondinoJuan@users.noreply.github.com>
2026-02-13 15:12:29 -0300

    Arregla render principal habilitando RouterOutlet

--- Frontend/Fantasy-Argentina/src/app/app.component.ts
+++ Frontend/Fantasy-Argentina/src/app/app.component.ts
@@ -1,27 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-/*
-interface Tournament {
-  name: string;
-  location: string;
-  startDate: string;
-}
-*/
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    //RouterOutlet,
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Fantasy Argentina';
-  idPagina: number = 0;
 }
