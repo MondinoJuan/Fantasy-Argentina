e953d365bfb9c519013b020f16ad5f92cfd19db6
Juan Cruz Mondino <58336253+MondinoJuan@users.noreply.github.com>
2026-02-19 21:43:07 -0300

    feat(backend): agregar servicio de última fecha con ratings

---
+++ backend/src/Entities/ExternalApi/services/index.ts
@@ -0,0 +1,6 @@
+export { getSportsApiProPlayerByIdService } from './getSportsApiProPlayerById.service.js';
+export { getSportsApiProPlayersByTeamService } from './getSportsApiProPlayersByTeam.service.js';
+export { getSportsApiProAllowedLeaguesService } from './getSportsApiProAllowedLeagues.service.js';
+export { getSportsApiProTeamsByLeagueService } from './getSportsApiProTeamsByLeague.service.js';
+export { getSportsApiProTeamDetailByTeamService } from './getSportsApiProTeamDetailByTeam.service.js';
+export { getLatestMatchdayResultsWithPlayerRatingsService } from './getLatestMatchdayResultsWithPlayerRatings.service.js';
