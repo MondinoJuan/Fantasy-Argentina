d3df66c9cbbb3bcda69fe772b5ed071dd0f18fe5
MondinoJuan <juancm.2000@hotmail.com>
2026-03-22 14:25:36 -0300

    agrego componente rivals-realPlayer-list

---
+++ Frontend/Fantasy-Argentina/src/app/components/rivals-real-player-list/rivals-real-player-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rivals-real-player-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './rivals-real-player-list.component.html',
+  styleUrl: './rivals-real-player-list.component.scss'
+})
+export class RivalsRealPlayerListComponent {
+
+}
